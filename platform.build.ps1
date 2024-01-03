988249bf6247fcd487bfbce8d44d5fcb74b94dfd
David Söderlund <ds@dsoderlund.consulting>
2024-01-03 14:14:03 +0100

    finished cluster setup for kind

--- platform.build.ps1
+++ platform.build.ps1
@@ -3,9 +3,38 @@ param (
 )
 
 task cert_up {
-    # generate an rsa key
-    openssl genrsa -out ./0_certs/root-ca/root-ca-key.pem 2048
-    openssl req -x509 -new -nodes -key ./0_certs/root-ca/root-ca-key.pem -days 3650 -sha256 -out ./0_certs/root-ca/root-ca.pem -subj "/CN=kube-ca"
-    Import-Certificate -FilePath "./0_certs/root-ca/root-ca.pem" -CertStoreLocation cert:\CurrentUser\Root
+    if(get-item 0_certs/root-ca -ea 0) {
+        throw "Root CA already exists"
+    }
+    else {   
+        # create a git ignored directory to store the root CA certificate and private key
+        mkdir 0_certs/root-ca | out-null
+        # create a new private key for the root CA
+        openssl genrsa -out ./0_certs/root-ca/root-ca-key.pem 2048 | out-null
+        # create a self-signed root CA certificate using the private key
+        openssl req -x509 -new -nodes -key ./0_certs/root-ca/root-ca-key.pem -days 3650 -sha256 -out ./0_certs/root-ca/root-ca.pem -subj "/CN=kube-ca" | out-null
+        # import the root CA certificate into the local machine's trusted root certificate store
+        Import-Certificate -FilePath "./0_certs/root-ca/root-ca.pem" -CertStoreLocation cert:\CurrentUser\Root
+    }
 }
 
+task cluster_up {
+    ctlptl apply -f 1_cluster/kind/cluster.yaml
+}
+task cluster_down {
+    ctlptl delete -f 1_cluster/kind/cluster.yaml
+}
+task local_dns {
+    write-host "copy and paste into your host files (need to save as admin)"
+@"
+############################################
+127.0.0.1 backstage.$dnsname
+127.0.0.1 kc.$dnsname
+127.0.0.1 argocd.$dnsname
+127.0.0.1 pg.$dnsname
+127.0.0.1 echo.$dnsname
+############################################
+"@ | write-host
+    code c:\windows\system32\drivers\etc\hosts
+}
+task init cert_up, local_dns
